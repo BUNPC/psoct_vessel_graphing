b1a165469366938fa3cb10b1e8ab48c030da2120
Mackenzie Hyman <mackenzie.hyman@gmail.com>
2023-05-03 17:04:02 -0400

    Specify buy-in computers in cluster

--- vasculature/SCC_main.sh
+++ vasculature/SCC_main.sh
@@ -1,11 +1,28 @@
 #!/bin/bash -l
 
+# Set SCC project
 #$ -P npbssmic
-#$ -pe omp 28
+
+# Specify job to run on lab's buy-in computers on SCC
+#$ -l buyin
+
+
+# Request a whole node with 28 cores and at least 384 GB of RAM.
+# Specify number of cores
+#$ -pe omp 16
+# Specify memory per core
 #$ -l mem_per_core=13G
+
+# Send email upon completion
 #$ -m ea
+
+# Time limit for job
 #$ -l h_rt=24:00:00
-#$ -N vessel_segmentation
+
+# Name of job
+#$ -N ves_seg_graph
+
+# Combine output/error files into single file
 #$ -j y
 
 module load matlab/2020b
