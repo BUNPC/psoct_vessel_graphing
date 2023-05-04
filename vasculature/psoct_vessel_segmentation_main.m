415f1fb68973fe1f78e191ccfe3ba9334c575229
Mackenzie Hyman <mackenzie.hyman@gmail.com>
2023-05-04 15:30:27 -0400

    Streamlined main script

--- vasculature/psoct_vessel_segmentation_main.m
+++ vasculature/psoct_vessel_segmentation_main.m
@@ -5,22 +5,22 @@
 % Detailed Description
 %{
 This script performs the following:
-- segmentation ()
-- convert segmentation to graph ()
+- segment the original volume
+- apply a mask to the segmentation
+- convert segmentation to graph
 - prune graph (remove loops and unterminated segments)
     - remove loops ()
     - remove segments ()
-- overlay graph and image
-- diameter
-- tortuosity (vessel_tortuosity_index.m)
-- length
-- topology (why does this use the mask?)
+
+To Do:
+- find optimal range for remove_mask_islands
 %}
 clear; clc; close all;
 
 %% Add top-level directory of code repository to path
 % This allows Matlab to find the functions in the project folders
-% Print current working directory
+
+% Start in current directory
 mydir  = pwd;
 % Find indices of slashes separating directories
 if ispc
@@ -28,88 +28,111 @@ if ispc
 elseif isunix
     idcs = strfind(mydir,'/');
 end
-% Remove the two sub folders to reach top-level directory
-% (psoct_vessel_graphing)
+% Truncate path to reach top-level directory (psoct_vessel_graphing)
 topdir = mydir(1:idcs(end));
 addpath(genpath(topdir));
 
+%% Automate segmentation of multiple volumes
+%{
+dpath = '/projectnb/npbssmic/ns/Ann_Mckee_samples_10T/';
+subid = {'AD_10382', 'AD_20832', 'AD_20969', 'AD_21354', 'AD_21424',...
+         'CTE_6489', 'CTE_6912', 'CTE_7019', 'CTE_8572', 'CTE_7126',...
+         'NC_21499', 'NC_6047', 'NC_6839', 'NC_6974', 'NC_7597',...
+         'NC_8095', 'NC_8653'};
+for ii = length(subid)
+    % Set voxel size from filename (either "_10ds" or "_4ds")
+
+    % Segment
+
+    % Mask
+
+    % Convert to graph
+
+    % Initialize graph structure information
+
+    % Downsample graph
+
+    % Prune loops
+
+    % Straighten segments
+end
+%}
 
 %% Import volume (.TIF or .BTF) & convert to MAT 
 
-%%% Local paths (windows PC)
-%{
-dpath = 'C:\Users\mack\Documents\BU\Boas_Lab\psoct_human_brain_resources\test_data\Hui_Frangi_dataset\200218depthnorm\';
-fname = 'volume_ori_inv_cropped';
-% filename extension
-ext = '.tif';
-filename = strcat(dpath, strcat(fname,ext));
-% Convert .tif to .MAT
-vol = TIFF2MAT(filename);
-%}
+% Check if running on local machine for debugging or on SCC for processing
+if ispc
+    %%% Local machine
+    dpath = 'C:\Users\mack\Documents\BU\Boas_Lab\psoct_human_brain_resources\test_data\Hui_Frangi_dataset\200218depthnorm\';
+    fname = 'volume_ori_inv_cropped';
+    % filename extension
+    ext = '.tif';
+    filename = strcat(dpath, strcat(fname,ext));
+    % Convert .tif to .MAT
+    vol = TIFF2MAT(filename);
+elseif isunix
+    %%% Computing cluster (SCC)
+    dpath = '/projectnb/npbssmic/ns/Ann_Mckee_samples_10T/AD_10382/dist_corrected/volume/';
+    fname = 'ref_4ds_norm';
+    % filename extension
+    ext = '.btf';
+    filename = strcat(dpath, strcat(fname,ext));
+    % Convert .tif to .MAT
+    vol = TIFF2MAT(filename);
+end
 
-%%% SCC paths
+%% Initialization parameters
 
-dpath = '/projectnb/npbssmic/ns/Ann_Mckee_samples_10T/AD_10382/dist_corrected/volume/';
-fname = 'ref_4ds_norm';
-% filename extension
-ext = '.btf';
-filename = strcat(dpath, strcat(fname,ext));
-% Convert .tif to .MAT
-vol = TIFF2MAT(filename);
-%}
-
-%% Set Voxel Size
 %%% Assign PS-OCT voxel dimension [x, y, z] according to downsample factor
 % Downasample factor = 4 --> Voxel = [12, 12, 15] micron
 % Downasample factor = 10 --> Voxel = [30, 30, 35] micron
-% TODO: Create function to extract downsample and voxel size from filename.
-%       The filename contains either "_10ds" or "_4ds".
+% 2P microscopy voxel will always be 5um x 5um
 
-% Temporary hard-coded voxel dimensions
-% PS-OCT voxel dimensions (microns)
+% PS-OCT voxel dimensions (microns) (hard coded for debugging)
 vox_dim = [30, 30, 35];
-
-%%% 2P microscopy voxel will always be 5um x 5um
-vox2p = [5, 5];
-
-%% Segment the volume
 % Std. Dev. for gaussian filter
 sigma = 1;
-% threshold to determine whether voxel belongs to a vessel. This is applied
-% to the probability matrix from the output of the frangi filter.
+% Threshold for Frangi filter probability matrix (voxel<thresh = not vessel)
 thresh = 0.2;
-% Segments with few than "min_conn" voxels will be removed
+% A segment with < "min_conn" voxels will be removed
 min_conn = 30;
-
-% Run segmentation function
-[I_seg] = ...
-    segment_main(dpath, fname, ext, sigma, thresh, min_conn);
-
-% Save segmentation before applying mask
-fname = strcat(fname,'_segment','_sigma', num2str(sigma));
-fout = strcat(dpath, fname, '.tif');
-segmat2tif(I_seg, fout);
-
-%% Apply mask to segmentation volume -- remove erroneous vessels
-% TODO: find optimal range for remove_mask_islands
 
 % Create 3D mask from original volume
 mask = logical(vol);
 % Array of radii for eroding the mask
-radii = 10:2:22;
+radii = 10:2:14;
 
-for ii = 1:length(radii)
-    %% Apply mask and save .MAT and .TIF
-    [I_seg_masked] = mask_segments(I_seg, mask, radii(ii), dpath, fname);
+% Boolean for converting segment to graph (0 = don't convert, 1 = convert)
+graph_boolean = 1;
 
-    %% Convert masked segmentation to graph
-    % Use masked segmentation to create graph
-    Graph = seg_to_graph(I_seg_masked, vox_dim);
+%% Segment the volume
+[I_seg, fname] = ...
+    segment_main(vol, sigma, thresh, min_conn, dpath, fname, ext);
+
+%% Mask segmented volume (remove erroneous vessels) & Convert to Graph
+% The function for creating the mask requires a radius. This for-loop will
+% iterate over an array of radii. For each radius, it will create a mask,
+% apply the mask to the segmentation volume, and save the output.
+% If the graph_boolean is true (1), then the masked segmentation will be
+% converted to a graph.
+
+for j = 1:length(radii)
+    %%% Apply mask and save .MAT and .TIF
+    [I_seg_masked] = mask_segments(I_seg, mask, radii(j), dpath, fname);
     
-    %% Create new filename for graph and add .MAT extension
-    tmp_fname = strcat(fname,'_masked_radius_', num2str(radii(ii)),'_graph.mat');
-    fout = strcat(dpath, tmp_fname);
-    save(fout,'Graph');
+    %%% Convert masked segmentation to graph
+    if graph_boolean
+        % Use masked segmentation to create graph
+        Graph = seg_to_graph(I_seg_masked, vox_dim);
+        
+        % initialize graph information
+%         Graph = initialize_graph(Graph);
+
+        % Create new filename for graph and add .MAT extension
+        tmp_fname = strcat(fname,'_masked_radius_', num2str(radii(j)),'_graph.mat');
+        fout = strcat(dpath, tmp_fname);
+        save(fout,'Graph');
+    end
 end
 
 %% Initialization of vesGraphValidate
@@ -163,35 +186,35 @@ save(fout, 'I_seg_masked', '-v7.3');
 end
 
 %% Segment volume
-function [I_seg] =...
-    segment_main(dpath, fname, ext, sigma, thresh, min_conn)
+function [I_seg, fname] =...
+    segment_main(vol, sigma, thresh, min_conn, dpath, fname)
 % Multiscale vessel segmentation
 %   INPUTS:
-%   sigma - vector of standard deviation values of gaussian filter to
+%       vol (matrix) - the original volume prior to segmentation
+%       sigma (array) - vector of std. dev. values of gaussian filter to
 %           calcualte hessian matrix at each voxel
-%   thres - threshold to determine which voxel belongs to a vessel. This is
-%           applied to the probability matrix from the output of the frangi
-%           filter.
-%   min_conn - vesSegment uses the function bwconncomp to determine the
+%       thres - threshold to determine which voxel belongs to a vessel.
+%           Applied to probability matrix from frangi filter output
+%       min_conn - vesSegment uses the function bwconncomp to determine the
 %               number of connected voxels for each segment. If the number
 %               of voxels is less than this threshold, then the segment
 %               will be removed.
+%       dpath (string) - absolute directory for saving processed data
+%       fname (string) - filename of segmentation
+%       ext (string) - filename extension (.TIF or .MAT)
 %   OUTPUTS:
 %       I_seg - segmentation of vessels (Frangi filter of original volume)
-%       I_seg_masked - masked segmentation (removed borders)
+%       fname (string) - upadted filename prior to applying mask
 
-%%% Load .TIF and convert to .MAT
-filename = strcat(dpath, strcat(fname,ext));
-vol = TIFF2MAT(filename);
-% convert volume to double matrix
+%% convert volume to double matrix
 I = double(vol);
 
 %%% Segment the original volume
 [~, I_seg] = vesSegment(I, sigma, thresh, min_conn);
 
-%%% Apply a mask to segmentation
-% Scalar for determining how much to erode mask
-% epsilon = 1;
-% I_seg_masked = mask_segments(I, I_seg, epsilon);
+%%% Save segmentation
+fname = strcat(fname,'_segment','_sigma', num2str(sigma));
+fout = strcat(dpath, fname, '.tif');
+segmat2tif(I_seg, fout);
 
 end
