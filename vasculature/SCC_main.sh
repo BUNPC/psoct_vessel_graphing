19c6a965f24e36b823b44f8e8e1eeb9c80089f6c
Mackenzie Hyman <mhyman@bu.edu>
2023-05-12 13:10:19 -0400

    Update SCC_main.sh

--- vasculature/SCC_main.sh
+++ vasculature/SCC_main.sh
@@ -3,10 +3,6 @@
 # Set SCC project
 #$ -P npbssmic
 
-# Specify job to run on lab's buy-in computers on SCC
-#$ -l buyin
-
-
 # Request a whole node with 28 cores and at least 384 GB of RAM.
 # Specify number of cores
 #$ -pe omp 16
@@ -20,11 +16,11 @@
 #$ -l h_rt=24:00:00
 
 # Name of job
-#$ -N ves_seg_graph
+#$ -N segment
 
 # Combine output/error files into single file
 #$ -j y
 
-module load matlab/2020b
+module load matlab/2022b
 matlab -nodisplay -singleCompThread -r "psoct_vessel_segmentation_main; exit"
 
