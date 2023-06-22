61c74d12e3b27063c94cb3556ff4c3b4ea2216a8
Mackenzie Hyman <mackenzie.hyman@gmail.com>
2023-06-22 18:23:55 -0400

    new function downsample_graph
     The down sampling logic will be rewritten. It will have an outer for loop to iterate over the segments. The inner for loop will iterate over nodes within this segment, and then down sample just these ndoes.

--- vasculature/vesGraphValidate/downsample_graph.m
+++ vasculature/vesGraphValidate/downsample_graph.m
@@ -1,14 +1,25 @@
 function [nodes, edges, validatedNodes,validatedEdges] =...
-    downsample_nodes(nodes, edges, validatedNodes, vox_xy, vox_z)
+    downsample_graph(nodes, edges, validatedNodes, vox_xy, vox_z)
 %%% Down sample 
 % INPUTS:
-%       nodes (matrix) - Nodes prior to downsample
-%       edges (matrix) - edges prior to downsample
-%       validatedNodes () - this is set to an array of zeros
-%       vox_xy (double) - voxel dimensions (x,y)
-%       vox_z (double) -  voxel dimension (z)
+%       nodes (matrix): Nodes prior to downsample
+%       edges (matrix): edges prior to downsample
+%       validatedNodes (array): validated node indices
+%       vox_xy (double): voxel dimensions (x,y)
+%       vox_z (double):  voxel dimension (z)
 % OUTPUTS:
-%       
+%       nodes (matrix): 
+%       edges (matrix): 
+%       validatedNodes (array): validated node indices
+%       validatedEdges (array): validated edges
+%{
+TODO:
+- rewrite this function so that the outer for-loop iterates over the
+segments. Within a segment, iterate over the nodes and compare the location
+of nodes within the boundaries [hxy, hxy, hz].
+%}
+
+
 
 %% Initialization
 % Set node position and edges
