eeb245f4c59d4225f65109e3622bd34afaa0501c
Mackenzie Hyman <mackenzie.hyman@gmail.com>
2023-07-20 18:25:11 -0400

    Test script for iterating subjects and gaussian

---
+++ vasculature/SCC_main_parallel_test.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -l
+
+# Set SCC project
+#$ -P npbssmic
+
+# Send email upon completion
+#$ -m ea
+
+# Time limit for job
+#$ -l h_rt=1:00:00
+
+# Name of job
+#$ -N test_array
+
+# Combine output/error files into single file
+#$ -j y
+
+### Declare array job (create new job for each)
+# There are 17 subjects and 3 gaussian sigma arrays
+# (small, medium, large) for a total of 51 jobs. 
+#$ -t 1-2
+
+# Keep track of information related to the current job
+echo "=========================================================="
+echo "Start date : $(date)"
+echo "Job name : $JOB_NAME"
+echo "Job ID : $JOB_ID  $SGE_TASK_ID"
+echo "=========================================================="
+
+module load matlab/2022b
+
+# Retrieve contents of line number $SGE_TASK_ID from the text file subject_sigma_index_list
+params=`sed -n "${SGE_TASK_ID} p" subject_sigma_index_list.txt`
+paramsArray=($params)
+
+# Assign first element of line to subject ID index
+subid_idx=${paramsArray[0]}
+
+# Assign second element of line to gaussian sigma array index
+gauss_idx=${paramsArray[1]}
+
+matlab -nodisplay -batch "psoct_vessel_segmentation_main" $subid_idx $gauss_idx
+
---
+++ vasculature/scc_parallel_test.m
@@ -0,0 +1,30 @@
+%% Test parallel batch array job on SCC
+% Author: Mack Hyman
+% Detailed Description
+%{
+This script performs the following:
+- pass in argument from bash script
+- print argument to command window
+%}
+clear; clc; close all;
+
+%% Input variable from bash script
+
+%%% Define subject ID based upon the subid_idx from the bash script
+subid_lst = {'AD_10382', 'AD_20832', 'AD_20969', 'AD_21354', 'AD_21424',...
+             'CTE_6489', 'CTE_6912', 'CTE_7019', 'CTE_8572', 'CTE_7126',...
+             'NC_21499', 'NC_6047', 'NC_6839', 'NC_6974', 'NC_7597',...
+             'NC_8095', 'NC_8653'};
+% subid_idx is an input parameter
+subid = subid_list{subid_idx}
+
+%%% Std. Dev. for gaussian filter
+% Define Gaussian sigma array based upon the gauss_idx from the bash script
+
+% The standard sigma array = [1, 3, 5]
+% Medium vessel sigma array = [7, 9, 11]
+% Large vessel sigma array = [13, 15, 17]
+sigma_lst = [1, 3, 5;...
+			7, 9, 11;...
+			13, 15, 17];
+gsigma = sigma_lst(gauss_idx,:)
