1dc4a4bc7e3ea3404d1a8f92e33524c5ecb96172
Mackenzie Hyman <mackenzie.hyman@gmail.com>
2023-08-16 13:03:26 -0400

    Final test script for move to mean
    - the move to mean function is reducing the diameter of loops but not completely collapsing them.

--- vasculature/vesGraphValidate/imView3d/mv_to_mean.m
+++ vasculature/vesGraphValidate/imView3d/mv_to_mean.m
@@ -1,24 +1,42 @@
 function im = mv_to_mean(im, Ithresh)
-% Move towards mean of neighboring nodes
+%% Move nodes towards mean of neighboring nodes
+% This script will iterate over every node index in the graph structure.
+% The comments reference "current node index," which is referring to
+% the ii'th node index in the list of total node indices.
+
+% Assign local variables
 nodes = im.nodes;
 nLst = 1:size(nodes,1);
 hwait = waitbar(0,'Moving nodes towards mean of neighboring nodes');
-for iii= 1:length(nLst) %1:nN
-    ii = nLst(iii);
-    waitbar(iii/length(nLst),hwait)
-    eLst = find(im.edges(:,1)==ii | im.edges(:,2)==ii);
-    nLst2 = setdiff(unique(im.edges(eLst,:)), ii);
+
+%% Iterate over nodes
+for ii = 1:size(nodes,1)
+    % Retrieve node index
+    nidx = nLst(ii);
+    waitbar(ii/length(nLst),hwait)
+    
+    %%%% Find edges containing current node index
+    eLst = find(im.edges(:,1)==nidx | im.edges(:,2)==nidx);
+    
+    %%% Find indices of nodes connected to current node index
+    % For the edges connected to the current node index, find the indices
+    % of other nodes connected to the same edge "unique(im.edges(eLst,:))"
+    % Use setdiff to exclude the index of the current node index (nidx).
+    nLst2 = setdiff(unique(im.edges(eLst,:)), nidx);
+
+    %%% Iterate over indices of connected nodes
     if length(nLst2)>1
-        pos0 = max(im.nodes(ii,:),1);
+        % Set null position to currrent node index or [1,1,1]
+        pos0 = max(im.nodes(nidx,:),1);
+        % Set C position to mean of connected nodes
         posC = mean(im.nodes(nLst2,:),1);
+        % New position = (null position + difference) / Euclidean distance
         posN = pos0 + (posC-pos0) / max(norm(posC-pos0),1);
-%            if im.angio(round(pos0(2)),round(pos0(1)),round(pos0(3)))>=Ithresh
-            if im.angio(round(posN(2)),round(posN(1)),round(posN(3)))>=Ithresh
-                nodes(ii,:) = posN;
-            end
-%            else
-%                nodes(ii,:) = posN;
-%            end
+        % If new position is within vessel (>=vox. intensity threshold)
+        % Then reassign node position to new position.
+        if im.angio(round(posN(1)),round(posN(2)),round(posN(3)))>=Ithresh
+            nodes(nidx,:) = posN;
+        end
     end
 end
 im.nodes = nodes;
--- vasculature/vesGraphValidate/test_mv2mean_recenter_regraph.m
+++ vasculature/vesGraphValidate/test_mv2mean_recenter_regraph.m
@@ -5,21 +5,34 @@
 % version of the function.
 
 % TODO: 
-%   - wrap for-loop around mv_to_mean
-%   - compare across iterations
+%   - iterate threshold
 
 clear; clc; close all;
 
-%% Setup filename + processing variables
+%% Add top-level directory of code repository to path
+% This allows Matlab to find the functions in the project folders
+
+% Start in current directory
+mydir  = pwd;
+% Find indices of slashes separating directories
+if ispc
+    idcs = strfind(mydir,'\');
+elseif isunix
+    idcs = strfind(mydir,'/');
+end
+% Truncate path to reach top-level directory (psoct_vessel_graphing)
+topdir = mydir(1:idcs(end));
+addpath(genpath(topdir));
+
+%% Setup filename
+% Paths to files
 dpath = 'C:\Users\mack\Documents\BU\Boas_Lab\psoct_data_and_figures\test_data\Ann_Mckee_samples_10T\';
 subid = 'NC_6839';
 subdir = '\dist_corrected\volume\';
 sigdir = 'gsigma_1-3-5_gsize_5-13-21\';
 vdata = 'ref_4ds_norm_inv_crop2.tif';
+% Graphd data after Gaussian filtering segmentation
 gdata = 'ref_4ds_norm_inv_crop2_segment_pmin_0.23_mask40_graph_data.mat';
-
-% Imaging voxel intensity threshold
-im_thresh = 52000;
 
 %% Load PSOCT graph
 tmp = load(fullfile(dpath, subid, subdir, sigdir, gdata), 'Data');
@@ -30,21 +43,43 @@ tmp = tmp.Data;
 im.nodes = tmp.Graph.nodes;
 im.edges = tmp.Graph.edges;
 
-%% Load volumetric information
+%% Load volumetric information and set threshold
+
+% Import volume
 vol = TIFF2MAT(fullfile(dpath, subid, subdir, vdata));
 im.angio = vol;
 
-%% Test move to mean
-im_mv = im;
-for i=1:10
-    im_mv = mv_to_mean(im_mv, im_thresh);
+% Imaging voxel intensity threshold (normalized [0,1])
+im_thresh = 0.75;
+
+% Check for data type
+if strcmp(class(vol),'uint16')
+    r = 65535;
+    im_thresh = im_thresh .* r;
+elseif strcmp(class(vol),'uint8')
+    r = 255;
+    im_thresh = im_thresh .* r;
 end
 
-%%% Compare results
+%% Test move to mean and thresholding
+th_norm = [0, 0.25, 0.5, 0.75, 0.9, 0.95];
+tarray = th_norm .* r;
+
 % Visualize uncentered graph
 graph_vis(im.nodes, im.edges, 'Graph Before Moving to Mean')
-% Visualize centered graph
-graph_vis(im_mv.nodes, im_mv.edges, 'Graph After Moving to Mean')
+
+for ii = 1:length(tarray)
+    im_mv = im;
+    for j=1:5
+        im_mv = mv_to_mean(im_mv, tarray(ii));
+    end
+    %%% Visualize centered graph
+    % Title for graph
+    t_str = num2str(th_norm(ii));
+    t_str = strcat('Voxel Intensity Threshold = ', t_str);
+    g_title = {'Graph After Moving to Mean', t_str};
+    graph_vis(im_mv.nodes, im_mv.edges, g_title)
+end
 
 %% Centering
 % Unsure what this variable does
