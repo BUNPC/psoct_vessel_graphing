91957ea0e719a9619c712db83e1c85a5bc09228a
Mackenzie Hyman <mackenzie.hyman@gmail.com>
2023-08-25 11:49:37 -0400

    XZ projection in overlay test script
    - this is for a figure for Stephan's final paper (2023)

--- vasculature/vessel_quality_assurance/overlay_vol_seg_test.m
+++ vasculature/vessel_quality_assurance/overlay_vol_seg_test.m
@@ -3,7 +3,7 @@
 The purpose of this script is to overlay the vessel segmentation and the
 PS-OCT volume (each slice).
 %}
-
+clear; clc; close all;
 %% Add top-level directory of code repository to path
 % This allows Matlab to find the functions in the project folders
 
@@ -27,21 +27,21 @@ if ispc
     
     % Volume directory + volume filename (same for each subject)
     voldir = '\dist_corrected\volume\';
-    volname = 'ref_4ds_norm_inv';
+    volname = 'ref_4ds_norm_inv_crop2';
 
     % Directory to segmentation
-    segdir = '\dist_corrected\volume\gsigma_13-15-17_gsize_53-61-69\';
+    segdir = '\dist_corrected\volume\gsigma_1-3-5_gsize_5-13-21\';
     % Subject IDs
     subid = {'NC_6839'};
     
     % Filename to parse (this is test data)
-    segname = 'ref_4ds_norm_inv_segment_pmin_0.23_mask40';
+    segname = 'ref_4ds_norm_inv_crop2_segment_pmin_0.23_mask40';
 
     % filename extension
     ext = '.tif';
 
 elseif isunix
-    error('Sorry, bruh. This is designed just for testing locally.')
+    error('This script was written to run just on windows.')
 end
 
 %% Overlay
@@ -55,19 +55,95 @@ for ii = 1:length(subid)
     % Convert .tif to .MAT
     seg = TIFF2MAT(filename);
     
-    %%% Load PS-OCT volume
+    %%% Load PS-OCT volume (inverted)
     fullpath = fullfile(dpath, subid{ii}, voldir);
     % Define filename of original ps-oct volume
     filename = strcat(fullpath, strcat(volname, ext));
     % Convert .tif to .MAT
-    vol = TIFF2MAT(filename);
+    voli = TIFF2MAT(filename);
     
     %%% Make filepath
     % Save output volume
     fullpath = fullfile(dpath, subid{ii}, segdir);
-    % Define filename of original ps-oct volume
+    % Define filename for output of overlay
     filename = strcat(fullpath, strcat(segname, '_overlay', ext));
     
-    %%% Call overlay function
-    overlay_vol_seg(vol, seg, 'green', filename);
+    %%% Call overlay function and save
+    overlay_vol_seg(voli, seg, 'green', filename);
+    
+    %% XZ stack projection (for Stephan's paper)
+%{
+    %%% Range for cropping y-axis
+    yrange = 200:300;
+
+    %%% Minimum intensity projection of volume (XZ from non-inverted)
+    % Convert from inverted to non-inverted
+    vol = imcomplement(voli);
+    % Crop Y to improve readability
+    vol = vol(:,yrange, :);
+    % Perform minimum intensity projection
+    minp = min(vol,[],2);
+    % Reshape into XZ
+    [x,~,z] = size(minp);
+    minp_re = reshape(minp, [x,z]);
+    % Transpose matrix so that X coordinate is on x-axis of figure
+    minp_re = minp_re';
+    % Create figure for debugging
+    figure; imagesc(minp_re); colormap(gray);
+    set(gca,'YDir','normal');
+
+
+    %%% Maximum intensity projection of segmentation (green)
+    % Crop Y to improve readability
+    seg = seg(:,yrange, :);
+    % Perform minimum intensity projection
+    maxp = max(seg,[],2);
+    % Reshape into XZ
+    [x,~,z] = size(maxp);
+    maxp_re = reshape(maxp, [x,z]);
+    % Transpose matrix so that X coordinate is on x-axis of figure
+    maxp_re = maxp_re';
+    % Create figure for debugging
+    figure; imagesc(maxp_re); colormap(gray);
+    set(gca,'YDir','normal');
+
+    %%% Overlay projections
+    ov = imoverlay(mat2gray(minp_re), maxp_re, 'green');
+    figure; imshow(ov);
+    % Update filename
+    filename = strcat(filename(1:end-12),'_XZ_overlay.tif');
+    imwrite(ov, filename);
+%}
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
