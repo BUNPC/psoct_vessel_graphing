60f3e18c4790c43e5b711248f9401fd0576c91b4
Mackenzie Hyman <mackenzie.hyman@gmail.com>
2024-03-08 12:46:28 -0500

    completed function to remove loops in parallel
    - works locally
    - need to add initialization to run on SCC
    - need to test on SCC with large dataset

--- vasculature/vesGraphValidate/rm_loops_parallel.m
+++ vasculature/vesGraphValidate/rm_loops_parallel.m
@@ -22,6 +22,19 @@ function [nodes, edges] = rm_loops_parallel(nodes, edges, angio, delta, v_min, m
 %   OUTPUTS:
 %       n ([n,3] array): node locations
 %       e ([m,2] array): edges connecting each node
+%
+%{
+% TODO:
+- determine order of operations for removing nodes in loops:
+    option 1:
+        - find loop nodes in entire graph:
+            [cnodes, cedges] = allcycles(g,'MaxNumCycles',500);
+        - run remove_reindex_nodes with both cnodes, nodes, and edges
+    option 2:
+        - separate graph into subgraphs
+        - check each subgraph for loops "hascycles"
+        - flag these to remove loops
+%}
 
 %% Initialize graph
 g = graph(edges(:,1), edges(:,2));
@@ -31,7 +44,7 @@ tf = hascycles(g);
 %% While graph conatins loops, continue removing them in parallel
 while tf
     % Separate into subgraphs, remove loops, recombine subgraphs
-    [nodes, edges] =...
+    [nodes, edges] = ...
         subgraph_rm(nodes, edges, angio, delta, v_min, mv_iter, viz, g);
 
     % Determine whether updated graph contains cycles (hascycles)
@@ -42,39 +55,155 @@ while tf
     % this function will return to parent call.
 end
 
+sprintf('Finished Removing Loops')
 
-function [n, e] = subgraph_rm(nodes, edges, angio, delta, v_min, mv_iter, viz, g)
+
+function [nodes, edges] = subgraph_rm(nodes, edges, angio, delta, v_min, mv_iter, viz, g)
     %SUBGRAPH_RM create subgraphs, remove loops, recombine into graph
     
     % Debugging variables
     tmp = []; tmp2 = [];
-    % Find loops in graph. Include a maximum to remain within memory limit
-    [cnodes, cedges] = allcycles(g,'MaxNumCycles',500);
-    cnodes_lst = horzcat(cnodes{:});
 
     %%% Find connected components (subgraphs)
     % This will classify nodes into the same "bin" index if they are
-    % connected. The "weak" type ensures that connected branches all have
-    % the same bin index.
+    % connected in the graph. The "weak" type ensures that connected
+    % branches all have the same bin index.
     bins = conncomp(g);
-   
-    %%% Find subgraphs containing loops (compare cnodes and bins)
-    % Create an array of subraph indices containing nodes in loops
-    subgraph_idcs = bins(cnodes_lst);
-    % Identify the unique subgraph indices
-    sub_idcs_uq = unique(subgraph_idcs);
+    % The unique number of bins is the total number of subgraphs
+    subgraph_idcs = unique(bins);
     
-    %%% Separate graph into subgraphs
-    
-    
-    %%% Store loop subgraphs in struct
+    %%% Separate graph into subgraphs (re-index nodes/edges)
+    subgraphs = struct();
+    % Create subgraph for each bin index
+    for ii = 1:length(subgraph_idcs)
+        % Find node indices with bin value of ii. In "nodeidx" the nodes
+        % with a bin value equal to ii will be set to 1 and the others will
+        % be set to 0.
+        nodeidx = bins == ii;
 
+        % Create list of node indices to remove. These have a bin value
+        % different from ii.
+        rm_idcs = 1:size(nodes,1);
+        rm_idcs = rm_idcs(~nodeidx);
+        
+        % Reindex nodes/edges for subgraph
+        [nodes_sub, edges_sub] = remove_reindex_nodes(rm_idcs, nodes, edges);
+        n_nodes = length(nodes_sub);
+        n_edges = length(edges_sub);
 
-    %%% Create parallel threads to remove loops
+        % Check if subgraph contains loops
+        subg = graph(edges_sub(:,1), edges_sub(:,2));
+        loop_tf = hascycles(subg);
+
+        % Place nodes/edges into struct
+        subgraphs(ii).nodes = nodes_sub;
+        subgraphs(ii).edges = edges_sub;
+        subgraphs(ii).loop_tf = loop_tf;
+        subgraphs(ii).n_nodes = n_nodes;
+        subgraphs(ii).n_edges = n_edges;
+    end
     
     
-    %%% Recombine subgraphs (after loop removal) and subgraphs (w/o loops)
+    %%% identify subgraph w/ loops (from struct)
+    sub_idx = [subgraphs.loop_tf].';
+    % struct index of subgraph containing loops
+    sub_idx = find(sub_idx);
+    L = length(sub_idx);
+    
+    %% Remove loops from subgraphs
+    % Initialize new struct for storing subgraphs after loop removal
+    subg_rm = subgraphs(sub_idx);
+
+    parfor j = 1:L
+        % Extract subgraph nodes/edges
+        sub_nodes = subg_rm(j).nodes;
+        sub_edges = subg_rm(j).edges;
+
+        % Remove loops in subgraph
+        [nodes_sub_rm, edges_sub_rm] = ...
+            rm_loops(sub_nodes, sub_edges, angio, delta, v_min, mv_iter, viz);
+        
+        % Find number of nodes and edges
+        n_nodes = size(nodes_sub_rm, 1);
+        n_edges = size(edges_sub_rm, 1);
+        
+        % Verify loops were removed
+        subg = graph(edges_sub_rm(:,1), edges_sub_rm(:,2));
+        loop_tf = hascycles(subg);
+        
+        % Add to struct
+        subg_rm(j).nodes = nodes_sub_rm;
+        subg_rm(j).edges = edges_sub_rm;
+        subg_rm(j).loop_tf = loop_tf;
+        subg_rm(j).n_nodes = n_nodes;
+        subg_rm(j).n_edges = n_edges;
+    end
+    
+    %% Recombine subgraphs (after loop removal) and subgraphs (w/o loops)
+    
+    %%% Move subg_rm back into subgraphs struct
+    for ii = 1:L
+        subgraphs(sub_idx(ii)) = subg_rm(ii);
+    end
+    
+    %%% Initialize arrays for combining all nodes/edges from all subgraphs
+    % into a single graph
+    n_nodes = sum([subgraphs.n_nodes]);
+    n_edges = sum([subgraphs.n_edges]);
+    nodes = zeros(n_nodes, 3);
+    edges = zeros(n_edges, 2);
+
+    % Init variables for calculating the offset for node_rm
+    n_idx0 = 1;
+    e_idx0 = 1;
+    e_offset = 0;
+
+    % Iterate over subgraphs
+    for ii = 1:length(subgraph_idcs)
+        % Extract nodes/edges from subgraph ii
+        nodes_sub = subgraphs(ii).nodes;
+        edges_sub = subgraphs(ii).edges;
+        
+        % Calculate end node indices
+        n_idxf = n_idx0 + size(nodes_sub, 1) - 1;
+        e_idxf = e_idx0 + size(edges_sub, 1) - 1;
+        
+        % Add subgraph nodes and edges to main list for entire graph
+        nodes(n_idx0:n_idxf,:) = nodes_sub;
+        edges(e_idx0:e_idxf,:) = edges_sub + e_offset;
+        
+        % Increment array indices
+        n_idx0 = n_idxf + 1;
+        e_idx0 = e_idxf + 1;
+        
+        % Increment edge offset by the number of nodes in the current
+        % subgraph.
+        e_offset = e_offset + size(nodes_sub,1);
+    end
+
+    % Visualize graph    
+    visualize_graph(nodes, edges, 'Graph after loop removal',[])
+    
 
 end
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
