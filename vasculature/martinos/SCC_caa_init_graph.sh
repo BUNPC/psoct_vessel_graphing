55565c8fec8133ceae99ae0cb8cf11601576b415
mack <mhyman@bu.edu>
2024-04-25 13:45:37 -0400

    scripts to initialize graphs from CAA data
    - excluded loop removal due to performance issues

---
+++ vasculature/martinos/SCC_caa_init_graph.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+
+# Set SCC project
+#$ -P npbssmic
+
+# Request a whole node with 28 cores and at least 384 GB of RAM.
+# Specify number of cores
+#$ -pe omp 4
+# Specify memory per core
+#$ -l mem_per_core=16G
+
+# Send email upon completion
+#$ -m ea
+
+# Time limit for job
+#$ -l h_rt=6:00:00
+
+# Name of job
+#$ -N caa_g
+
+# Combine output/error files into single file
+#$ -j y
+
+# Batch array
+#$ -t 1
+
+echo "Starting task number $SGE_TASK_ID"
+module load matlab/2022b
+matlab -nodisplay -batch caa_init_graph $SGE_TASK_ID
