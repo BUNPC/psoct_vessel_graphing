834eaa8db6f7d605d395f5c5ecb8b2d0c16bcb03
mack <mhyman@bu.edu>
2024-05-02 11:08:45 -0400

    Branch density - take logical of tissue mask
    - Take logical of tissue mask to only count the number of tissue voxels.

--- vasculature/vessel_analyses/branch_density.m
+++ vasculature/vessel_analyses/branch_density.m
@@ -1,9 +1,23 @@
-function branchden = branch_density(data, t_mask)
-% volume is t_vol
+function bd = branch_density(data, t_mask)
+%% Calculate the branch density in 1/microns^3
+% Find all non-zero voxels in tissue volume, convert to logical, take sum
+% to identify total number of tissue voxels, then convert to cubic microns.
+% Then, divide the total number of branchesby the total volume.
+%
+% INPUTS:
+%   data (struct): data structure of graph
+%   t_mask (uint8): tissue mask
+% OUTPUTS:
+%   bd (double): branch density
 
-    vox = data.Graph.vox;
-    vox_vol = vox(1) .* vox(2) .* vox(3);   % Creat function to calculate tissue volume
-    volume = sum(t_mask(:)) .* vox_vol;
-    nb = data.Graph.nB;
-    branchden = sum(nb) / volume;
+% Retrieve voxel dimensions (x,y,z)
+vox = data.Graph.vox;
+% Calculate volume of a single voxel
+vox_vol = vox(1) .* vox(2) .* vox(3);
+% Calculate volume of tissue from the non-zero voxels in tissue mask
+t_vol = sum(logical(t_mask(:))) .* vox_vol;
+% Retrieve number of branch points
+nb = data.Graph.nB;
+% Calculate branch density
+bd = sum(nb) / t_vol;
 end
--- vasculature/vessel_analyses/length_density.m
+++ vasculature/vessel_analyses/length_density.m
@@ -1,8 +1,8 @@
 function ld = length_density(data, t_mask)
 %% Calculate the length density in 1/microns^2
 % Find all non-zero voxels in tissue volume, convert to logical, take sum
-% to identify total number of tissue voxels, then convert to voxels. Then,
-% divide the total length of all vessels by the total volume.
+% to identify total number of tissue voxels, then convert to cubic microns.
+% Then, divide the total length of all vessels by the total volume.
 %
 % INPUTS:
 %   data (struct): data structure of graph
@@ -10,10 +10,14 @@ function ld = length_density(data, t_mask)
 % OUTPUTS:
 %   ld (double): length density
 
-    vox = data.Graph.vox;
-    vox_vol = vox(1) .* vox(2) .* vox(3);
-    t_vol = sum(logical(t_mask(:))) .* vox_vol;
-    ld = sum(data.Graph.segInfo.segLen_um) ./ t_vol;
+% Retrieve voxel dimensions (x,y,z)
+vox = data.Graph.vox;
+% Calculate volume of a single voxel
+vox_vol = vox(1) .* vox(2) .* vox(3);
+% Calculate volume of tissue from the non-zero voxels in tissue mask
+t_vol = sum(logical(t_mask(:))) .* vox_vol;
+% Calculate length density from sum of all segments and total tissue vol
+ld = sum(data.Graph.segInfo.segLen_um) ./ t_vol;
 end
 
     
