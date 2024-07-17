0f95419bc59bebd21d2e71f4512a45cdb4d23f54
mack <mhyman@bu.edu>
2024-07-17 12:32:49 -0400

    function - kruskal-wallis test + dunnett's multi-comp
    This function performs the kruskal-wallis test on AD, CTE, and NC. Then, it applies the dunnett's multiple comparisons test to determine if there is significance between either of the two experimental groups and the control group.

---
+++ vasculature/vessel_analyses/kw_dunnett.m
@@ -0,0 +1,44 @@
+function [p] = kw_dunnett(ad, cte, nc)
+%KW_DUNNETT Kruskal-Wallis followed by multi comparisons (Dunnett's)
+%   This is an example function of how to compare multiple experimental
+%   groups to the same control group. The Kruskal-Wallis tests whether each
+%   dataset comes from the same distribution. The Dunnett's multiple
+%   comparisons method corrects for multiple comparisons between multiple
+%   experimental groups with the same control group.
+% INPUTS:
+%   ad (double array): the values from the AD group
+%   cte (double array): the values from the CTE group
+%   nc (double array): the values from the NC group
+% OUTPUTS
+%   p (double array): the p-values for the two experimental groups compared
+%       to the control group. p(1) is the p-value for AD. p(2) is the
+%       p-value for CTE.
+
+%% Initialize variables
+% array for storing P-value
+p = zeros(2,1);
+
+%%% Create labels for each vector of [experimental; control]
+[nc_group{1:size(nc)}] = deal('nc');
+[ad_group{1:size(ad)}] = deal('ad');
+[cte_group{1:size(cte)}] = deal('cte');
+% Combine all group labels into a single array. This is used in teh
+% kruskal-wallis test for assigning each data point to a group
+group = [ad_group, cte_group, nc_group]';
+
+%%% Concatenate the experimental & control vectors into a matrix
+% This is the input data for the kruskal-wallis test
+ad_cte_nc = [ad; cte; nc];
+
+%% Call the Kruskal Wallis Test
+[~,~,stats] = kruskalwallis(ad_cte_nc, group);
+% Multiple Comparisons w/ Dunnett correction
+[results,~,~,~] = multcompare(stats,...
+    'ControlGroup',3,...
+    'CriticalValueType','dunnett');
+% Extract the p-values for each comparison
+p(1) = results(1,6);
+p(2) = results(2,6);
+
+
+end
